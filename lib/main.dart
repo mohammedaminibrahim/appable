fb966bd7c619a1e4e0835b0a13cae14f300c393f
Mohammed Amin Ibrahim <mohammedaminibrahim10@gmail.com>
2023-04-21 21:39:53 +0100

    Working on how to set theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,73 +1,42 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const App());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Main Page'),
+      theme: ThemeData(),
+      darkTheme: ThemeData(),
+      home: AppHome()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
+class AppHome extends StatelessWidget {
+  const AppHome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+      appBar: AppBar(title: const Text(".appable/"), leading: const Icon(Icons.ondemand_video),),
+      floatingActionButton: FloatingActionButton(child: const Icon(Icons.add_shopping_cart), onPressed: (){}),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(
+          children: [
+            const Text("Heading"),
+            const Text("Sub-Heading"),
+            const Text("Paragraph"),
+            ElevatedButton(onPressed: () {}, child: const Text("Elevated Button")),
+            OutlinedButton(onPressed: () {}, child: const Text("Outlined Button")),
+            const Padding(padding: EdgeInsets.all(20.0),
+            child: Image(image: AssetImage("assets/images/book.jpg")),)
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
