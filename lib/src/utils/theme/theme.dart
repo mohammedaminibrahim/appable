2ad2e2668c2896b89f4998bfba08b60f065ca868
Mohammed Amin Ibrahim <mohammedaminibrahim10@gmail.com>
2023-04-23 11:49:13 +0100

    Done With Theme Management

--- lib/src/utils/theme/theme.dart
+++ lib/src/utils/theme/theme.dart
@@ -1,6 +1,19 @@
+import 'package:firstapp/src/utils/theme/text_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 class TAppTheme {
-  static ThemeData lightTheme = ThemeData(brightness: Brightness.light);
-  static ThemeData darkTheme = ThemeData(brightness: Brightness.dark);
+
+  //Prevent useer to access the theme using the instances of the class
+  TAppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+      brightness: Brightness.light,
+      textTheme: TTextTheme.lightTextTheme
+  );
+  static ThemeData darkTheme = ThemeData(
+      brightness: Brightness.dark,
+      textTheme: TTextTheme.darkTextTheme
+  );
 }
