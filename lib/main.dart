bbe19d910ee8e62388933a1cad765229277a807d
Mohammed Amin Ibrahim <mohammedaminibrahim10@gmail.com>
2023-04-23 10:37:54 +0100

    Theme Color to text added Successfully

--- lib/main.dart
+++ lib/main.dart
@@ -25,15 +25,16 @@ class AppHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //backgroundColor: Theme.of(context).primaryColor,
       appBar: AppBar(title: const Text(".appable/"), leading: const Icon(Icons.ondemand_video),),
       floatingActionButton: FloatingActionButton(child: const Icon(Icons.add_shopping_cart), onPressed: (){}),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: ListView(
           children: [
-            const Text("Heading"),
-            const Text("Sub-Heading"),
-            const Text("Paragraph"),
+             Text("Heading", style: Theme.of(context).textTheme.headline2,),
+             Text("Sub-Heading", style: Theme.of(context).textTheme.subtitle2,),
+             Text("Paragraph", style: Theme.of(context).textTheme.bodyText2,),
             ElevatedButton(onPressed: () {}, child: const Text("Elevated Button")),
             OutlinedButton(onPressed: () {}, child: const Text("Outlined Button")),
             const Padding(padding: EdgeInsets.all(20.0),
